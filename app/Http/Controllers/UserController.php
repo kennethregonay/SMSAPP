985ec03faca047d2f188ca0b0c18a935a09c2fc4
Kenneth Regonay <57382406+kennethregonay@users.noreply.github.com>
2022-06-08 02:35:55 +0800

    Co-authored-by: navera.johnarnulfo@gmail.com <navera.johnarnulfo@gmail.com> Co-authored-by: NotYourOrdinaryGirl <SimpleCycy@users.noreply.github.com> Co-authored-by: AnimeAngel138 <AnimeAngel138@users.noreply.github.com>

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,19 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    //
-
-    public function index()
+   
+    // SIGN UP IS RUNNING
+    public function create()
     {
-        return view('welcome');
+        $request = request()->validate([
+            'name' => 'required',
+            'gender' => 'required',
+            'position' => 'required',
+            'email' => 'required|unique:users,email',
+            'password' => 'required|min:8|max:20',
+            'type' => 'required',
+        ]);
+
+        $user = new User();
+        $user['name'] = $request['name'];
+        $user['gender'] = $request['gender'];
+        $user['position'] = $request['position'];
+        $user['email'] = $request['email'];
+        $user['password'] = bcrypt($request['password']);
+        $user['type'] = $request['type'];
+        $user->save();
+
+        return redirect('test');
+    }
+    // LOGIN RUNNING
+    public function login()
+    {
+        $request = request()->validate([
+            'email' => 'required',
+            'password' => 'required|min:8|max:20',
+        ]);
+
+        if (Auth()->attempt($request)) {
+            return redirect('dashboard');
+        }
+        return redirect('/');
     }
 
-    public function create (){
-        $request = Request()->all();
-        dd($request);
+    // LOGOUT RUNNING
+    public function logout (){
+        auth()->logout();
+        return redirect('/')->with('success');
     }
 }
