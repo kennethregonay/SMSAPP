a81480ec896c0352dffb68f4e12fcaa6d6840534
Kenneth Regonay <57382406+kennethregonay@users.noreply.github.com>
2022-06-10 09:34:34 +0800

    Co-authored-by: navera.johnarnulfo@gmail.com <navera.johnarnulfo@gmail.com> Co-authored-by: AnimeAngel138 <AnimeAngel138@users.noreply.github.com> Co-authored-by: NotYourOrdinaryGirl <SimpleCycy@users.noreply.github.com>

--- resources/views/registerformlist.blade.php
+++ resources/views/registerformlist.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <div class="container-fluid">
-        <h3 class="mt-3">Registered Form List</h3>
+        <h3 class="mt-3">Pre-Registered Form List</h3>
         <hr style="height: 4px;color: rgb(0,0,0);">
         <div class="card">
             <div class="card-body">
@@ -9,77 +9,37 @@
                     <thead>
                         <tr>
                             <th hidden style="text-align: center;">ID</th>
-                            <th style="width: 15%; text-align: center;">NAME</th>
-                            <th style="width: 15%; text-align: center;">ENROLLING GRADE LEVEL</th>
-                            <th style="width: 15%; text-align: center;">STATUS</th>
-                            <th style="width: 15%; text-align: center;">DATE</th>
-                            <th style="width: 15%; text-align: center;">ACTION</th>
+                            <th style="text-align: center;">REFERENCE NO.</th>
+                            <th style="text-align: center;">NAME</th>
+                            <th style="text-align: center;">ENROLLING GRADE LEVEL</th>
+                            <th style="text-align: center;">GWA</th>
+                            <th style="text-align: center;">STATUS</th>
+                            <th style="text-align: center;">ACTION</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td hidden style="text-align: center;">ID</td>
-                            <td style="text-align: center;">Joshua P. Padua</td>
-                            <td style="text-align: center;">Grade 6</td>
-                            <td style="text-align: center;">Pending</td>
-                            <td style="text-align: center;">20/10/022</td>
-                            <td style="text-align: center;">
-                                <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                                    data-bs-target="#view">View</button>
+                        @foreach ($learners as $student)
+                            <tr></tr>
+                                <td hidden>{{ $student->id }}</td>
+                                <td>{{ $student->RefNo }}</td>
+                                <td>{{ $student->fname }} {{ $student->mname }} {{ $student->lname }}</td>
+                                <td style="text-align: center;">{{ $student->glevel }}</td>
+                                <td>{{ $student->GWA }}</td>
+                                <td>{{ $student->EnrollmentStatus }}</td>
+                                <td><button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                    data-bs-target="#viewLeaners{{ $student->id }}">View</button>
                                 <button type="button" class="btn btn-success" data-bs-toggle="modal"
-                                    data-bs-target="#confirm">Accept</button>
+                                    data-bs-target="#approveRegistration{{ $student->id }}">Accept</button>
                                 <button type="button" class="btn btn-danger" data-bs-toggle="modal"
-                                    data-bs-target="#confirm">Decline</button>
-                            </td>
-                        </tr>
+                                    data-bs-target="#declineRegistration{{ $student->id }}">Decline</button></td>
+                            </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
-
-    {{-- Confirm Request --}}
-    <div class="modal" id="confirm">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h2 class="modal-title">Confirmation</h2>
-                    <button type="button" class="close" data-bs-dismiss="modal">&times;</button>         
-                </div>
-                <div class="modal-body">
-                    <form action="" method="">
-                        <div class="mb-2">
-                            <p>Are you Sure you want to continue?</p>
-                        </div>
-
-                        <div class="modal-footer">
-                            <button class="btn btn-success" type="submit" data-bs-dismiss="modal">Confirm</button>
-                            <button class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="modal" id="view">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h2 class="modal-title">Register Form View</h2>
-                    <button type="button" class="close" data-bs-dismiss="modal">&times;</button>
-                </div>
-                <div class="modal-body">
-                    <form action="" method="">
-                        <div class="mb-2">
-                        </div>
-                        <div class="modal-footer">
-                            <button class="btn btn-success" type="submit" data-bs-dismiss="modal">Confirm</button>
-                            <button class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('modals.approveRegistration')
+    @include('modals.declineRegistration')
+    @include('modals.viewRegistration')
 @endsection
