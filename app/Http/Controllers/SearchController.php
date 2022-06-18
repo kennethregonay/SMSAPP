8a2b4224b283e3105fd13cb89417a3db71c46a55
Kenneth Regonay <57382406+kennethregonay@users.noreply.github.com>
2022-06-18 23:16:22 +0800

    Co-authored-by: AnimeAngel138 <AnimeAngel138@users.noreply.github.com>

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    //
+
+    public function searchAccount(){
+        // dd(Request()->all());
+
+        $accounts = User::where('status' , '=', 'Active')->get();
+        $search = Request()->validate([
+            'search' => 'required'
+        ]);
+
+        if($search){
+            
+        }
+      
+
+
+    }
+
+
+
+
+}
