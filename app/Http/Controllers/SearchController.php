5a7965db5c600e1e05cd84e7b9e52f9a9ef511f2
Kenneth Regonay <57382406+kennethregonay@users.noreply.github.com>
2022-06-19 13:14:48 +0800

    updates un search functionalities

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -12,20 +12,39 @@ class SearchController extends Controller
     public function searchAccount(){
         // dd(Request()->all());
 
-        $accounts = User::where('status' , '=', 'Active')->get();
+        $accounts = User::where('status' , '=', 'Active');
         $search = Request()->validate([
             'search' => 'required'
         ]);
 
         if($search){
             
-        }
-      
+           
+    
+        } 
+           
+    
+    
+    }
 
+        
+        
+        
+        
+        
+           private function getAccount($accounts, $search){
+            $acc = $accounts->where('email' , 'like', '%'.$search['search'].'%')
+            ->where('role' , 'like', '%'.$search['search'].'%')
+            ->where('status' , 'like', '%'.$search['search'].'%')
+            ->where('type' , 'like', '%'.$search['search'].'%');         
+
+            return view('account', ['accounts' => $acc]);
+            return view('account', ['accounts' => $accounts]);
+        }
 
     }
 
 
 
 
-}
+
